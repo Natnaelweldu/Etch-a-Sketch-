06c4fbae3ed835c5903fef80a5128690b1642c1e
Natnael Woldu <natnael.Welduu@gmail.com>
2024-09-09 22:47:04 +0300

    Make the sketch pad interactive by adding hover effect
    add applyHoverEffect and removeHoverEffect function by listening to mouse over event and remove it after 0.5 sec

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const container = document.querySelector(".container");
 const gridArray = [];
+let row = 16;
+let col = 16;
 
 function createGrid() {
-  for (let i = 0; i <= 16; i++) {
+  for (let i = 0; i <= row; i++) {
     gridArray[i] = [];
-    for (let j = 0; j <= 16; j++) {
+    for (let j = 0; j <= col; j++) {
       const newBox = document.createElement("div");
       gridArray[i][j] = newBox;
       newBox.classList.add("box");
       container.appendChild(newBox);
-      console.log(gridArray[i][j]);
     }
   }
 }
 
 createGrid();
+
+function applyHoverEffect(element) {
+  element.classList.add("hovering");
+}
+function removeHoverEffect(element) {
+  element.classList.remove("hovering");
+}
+
+const hoveringElement = document.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("box")) {
+    applyHoverEffect(e.target);
+  }
+});
+
+document.addEventListener("mouseover", (e) => {
+  setTimeout(() => {
+    removeHoverEffect(e.target);
+  }, 500);
+});
