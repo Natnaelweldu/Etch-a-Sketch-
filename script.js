4740e85050333eda76ed8e8bae3a5e8728b7bb50
Natnael Woldu <natnael.Welduu@gmail.com>
2024-09-11 22:11:59 +0300

    Change the style and refactor the code
    change the style to look more like a sketch pad and refactor the code removing the array as we don't need need it and reduced huge amount of unused or repeating code

--- script.js
+++ script.js
@@ -1,66 +1,47 @@
 const container = document.querySelector(".container");
-const gridArray = [];
-// let rowCol = 16;
+const gridInput = document.querySelector("#input");
+const gridSizeBoard = document.querySelector("#gridSizeBoard");
+const resetBtn = document.querySelector("#resetBtn");
 
-function createGrid(rowCol) {
-  for (let i = 0; i <= rowCol; i++) {
-    gridArray[i] = [];
-    for (let j = 0; j <= rowCol; j++) {
-      if (container.scrollHeight > container.clientHeight) {
-        console.log("The container is overflowing!");
-        return;
-      } else {
-        const newBox = document.createElement("div");
-        gridArray[i][j] = newBox;
-        newBox.classList.add("box");
-        newBox.style.width = screen.width / rowCol + "px";
-        newBox.style.height = screen.height / rowCol + "px";
-        container.appendChild(newBox);
-      }
-    }
-  }
-}
-
-createGrid(16);
-
-function applyHoverEffect(element) {
-  element.style.backgroundColor = getRandomRGB();
-}
-function removeHoverEffect(element) {
-  element.style.backgroundColor = "#fff";
-}
-
-const hoveringElement = document.addEventListener("mouseover", (e) => {
-  if (e.target.classList.contains("box")) {
-    applyHoverEffect(e.target);
-  }
-});
-
-document.addEventListener("mouseover", (e) => {
-  setTimeout(() => {
-    removeHoverEffect(e.target);
-  }, 500);
-});
-
-function clearElements() {
+function createGrid(size) {
   container.innerHTML = "";
+  const boxSize = 500 / size; // 500px is the container size
+
+  for (let i = 0; i < size * size; i++) {
+    const newBox = document.createElement("div");
+    newBox.classList.add("box");
+    newBox.style.width = `${boxSize}px`;
+    newBox.style.height = `${boxSize}px`;
+    container.appendChild(newBox);
+  }
+}
+
+function updateGrid() {
+  const size = parseInt(gridInput.value);
+  createGrid(size);
+  gridSizeBoard.textContent = `${size} x ${size}`;
 }
 
 function getRandomRGB() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-
   return `rgb(${r},${g},${b})`;
 }
 
-document.addEventListener("DOMContentLoaded", (e) => {
-  const gridInput = document.querySelector("#input");
-  const btn = document.querySelector("#btn");
-  btn.addEventListener("click", (e) => {
-    const body = document.querySelector("body");
-    clearElements();
-    const rowCol = gridInput.value;
-    createGrid(rowCol);
+container.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("box")) {
+    e.target.style.backgroundColor = getRandomRGB();
+  }
+});
+
+gridInput.addEventListener("input", updateGrid);
+
+resetBtn.addEventListener("click", () => {
+  document.querySelectorAll(".box").forEach((box) => {
+    box.style.backgroundColor = "";
   });
 });
+
+// Initial grid creation
+updateGrid();
