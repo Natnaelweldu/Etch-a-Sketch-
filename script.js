e34d00563d4427d76fb9aca59b01e2f5eea8b01b
Natnael Woldu <natnael.Welduu@gmail.com>
2024-09-11 11:37:39 +0300

    Add the grid size button that make the creating grid dynamic
    add an event listener for when the dom is finished loading and then hear what the user value for dynamic allocation

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const container = document.querySelector(".container");
 const gridArray = [];
-let row = 16;
-let col = 16;
+// let rowCol = 16;
 
-function createGrid() {
-  for (let i = 0; i <= row; i++) {
+function createGrid(rowCol) {
+  for (let i = 0; i <= rowCol; i++) {
     gridArray[i] = [];
-    for (let j = 0; j <= col; j++) {
+    for (let j = 0; j <= rowCol; j++) {
       const newBox = document.createElement("div");
       gridArray[i][j] = newBox;
       newBox.classList.add("box");
+      newBox.style.width = screen.width / rowCol + "px";
+      newBox.style.height = screen.height / rowCol + "px";
       container.appendChild(newBox);
     }
   }
 }
 
-createGrid();
+createGrid(16);
 
 function applyHoverEffect(element) {
   element.classList.add("hovering");
@@ -34,4 +35,19 @@ document.addEventListener("mouseover", (e) => {
   setTimeout(() => {
     removeHoverEffect(e.target);
   }, 500);
+});
+
+function clearElements() {
+  container.innerHTML = "";
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const gridInput = document.querySelector("#input");
+  const btn = document.querySelector("#btn");
+  btn.addEventListener("click", (e) => {
+    const body = document.querySelector("body");
+    clearElements();
+    const rowCol = gridInput.value;
+    createGrid(rowCol);
+  });
 });
